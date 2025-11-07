b180bf9190f9ac42348c42d74d5e1cdd04fe44d9
MCJack123 <jackmacwindowslinux@gmail.com>
2025-11-07 02:47:23 -0500

    Testing OpenCL

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ class BuildCommand(build):
 
 if os.environ.get('CIBUILDWHEEL', '0') == '1' and platform.system() == "Windows":
     triplet = "x64-windows" if platform.machine() == "x86_64" else "arm64-windows"
-    assert os.access(os.environ.get("VCPKG_INSTALLATION_ROOT") + "\\installed\\" + triplet + "\\lib\\OpenCL.lib", os.F_OK)
+    assert os.path.exists(os.environ.get("VCPKG_INSTALLATION_ROOT") + "\\installed\\" + triplet + "\\lib\\OpenCL.lib")
     setup(
         cmdclass={"build_clcpp": build_clcpp, "build": BuildCommand},
         ext_modules=[Extension(
